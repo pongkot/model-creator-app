237b8dda90faa09a33a2c3560434a60ff21f5766
Pongkot Sae-li <pongkot.saeli@outlook.com>
2020-12-20 14:40:26 +0700

    add help

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-6">
-          <div class="col">
+          <div class="col text-center">
             <h1 class="mt-3">Model Creator</h1>
             <p>Create TypeScript model (getter, setter)</p>
           </div>
@@ -14,6 +14,7 @@
               class="form-control"
               id="className"
               v-model="className"
+              required
             />
           </div>
           <div class="mb-3">
@@ -21,14 +22,15 @@
             <textarea
               class="form-control"
               id="classProperties"
-              rows="7"
+              rows="8"
               v-model="classProperties"
+              required
             ></textarea>
           </div>
           <div class="mb-3">
             <button
               id="btnCreateModel"
-              type="button"
+              type="submit"
               class="btn btn-success w-100"
               @click="createModel"
             >
@@ -41,7 +43,8 @@
             >
             <input
               type="text"
-              class="form-control"
+              class="form-control readonly"
+              readonly
               id="exampleFormControlInput2"
               v-model="output"
             />
@@ -50,8 +53,16 @@
         <div class="col-6">
           <h3 class="mt-3">Help</h3>
           <hr />
-          className: {{ className }}<br />
-          classProperties: {{ classProperties }}
+          <ul>
+            <li>Fill class name</li>
+            <li>
+              Fill property name any type like this
+              <code>name:string, name: string</code>
+            </li>
+            <li>
+              Click Create
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -74,14 +85,17 @@ export default class App extends Vue {
   createModel() {
     const className = this.className;
     const classProps = this.classProperties;
-    const input: {
-      name: string;
-      properties: Array<{ name: string; type: string }>;
-    } = {
-      name: transform.toClassName(className),
-      properties: transform.toClassProperties(classProps)
-    };
-    this.output = parser.toStandardModel(input);
+
+    if (className !== "" && classProps !== "") {
+      const input: {
+        name: string;
+        properties: Array<{ name: string; type: string }>;
+      } = {
+        name: transform.toClassName(className),
+        properties: transform.toClassProperties(classProps)
+      };
+      this.output = parser.toStandardModel(input);
+    }
   }
 }
 </script>
@@ -91,7 +105,7 @@ export default class App extends Vue {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
 }
